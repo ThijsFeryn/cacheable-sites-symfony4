2297fd2c3b3ddb0a6f585e9982cf6b5ba0b0e8e7
ThijsFeryn <thijs@feryn.eu>
2018-04-27 15:40:33 +0200

    This is our starting point

--- config/bundles.php
+++ config/bundles.php
@@ -2,11 +2,8 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    SymfonyBundles\RedisBundle\SymfonyBundlesRedisBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -16,86 +16,13 @@ class DefaultController extends Controller
      */
     public function index()
     {
-        return $this
-            ->render('index.twig')
-            ->setPublic()
-            ->setMaxAge(100)
-            ->setSharedMaxAge(500);
-    }
-    /**
-     * @Route("/header", name="header")
-     */
-    public function header()
-    {
-        $response = $this
-            ->render('header.twig')
-            ->setPublic()
-            ->setMaxAge(100)
-            ->setSharedMaxAge(500);
-        return $response;
-    }
-    /**
-     * @Route("/footer", name="footer")
-     */
-    public function footer()
-    {
-        $response = $this->render('footer.twig')
-            ->setMaxAge(100)
-            ->setSharedMaxAge(500)
-            ->setPublic();
-        return $response;
-    }
-    /**
-     * @Route("/nav", name="nav")
-     */
-    public function nav(Request $request)
-    {
-        $response =  $this->render('nav.twig')
-            ->setVary('X-Login',false)
-            ->setMaxAge(100)
-            ->setSharedMaxAge(500)
-            ->setPublic();
-
-        return $response;
-    }
-    /**
-     * @Route("/login", name="login")
-     */
-    public function login(Request $request)
-    {
-        $response =  $this->render('login.twig',
-            [
-                'loginLogoutUrl' => $this->generateUrl('login'),
-                'loginLogoutLabel' => 'log_in',
-                'error' => $request->get('error')
-            ]
-        )
-            ->setVary('X-Login',false)
-            ->setMaxAge(100)
-            ->setSharedMaxAge(500)
-            ->setPublic();
-
-        return $response;
-    }
-    /**
-     * @Route("/logout", name="logout")
-     */
-    public function logout()
-    {
-        $response =  new RedirectResponse($this->generateUrl('login'));
-        $response->headers->clearCookie('token');
-        return $response;
+        return $this->render('index.twig');
     }
     /**
      * @Route("/private", name="private")
      */
-    public function private(Request $request)
+    public function private()
     {
-        $response =  $this->render('private.twig')
-            ->setVary('X-Login',false)
-            ->setMaxAge(100)
-            ->setSharedMaxAge(500)
-            ->setPublic();
-        return $response;
+        return $this->render('private.twig');
     }
 }
